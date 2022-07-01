bf98507611c6cc9ad88873a9fc86d4cdd5ac38d2
chenxiaomin-non <82016572+chenxiaomin-non@users.noreply.github.com>
2022-07-01 14:03:59 +0700

    Update define.py

--- define.py
+++ define.py
@@ -7,8 +7,8 @@ print(sys.path)
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-IP_ADDR = 'localhost'
-PORT = 8000
+IP_ADDR = '127.0.0.1'
+PORT = 8234
 
 HASH = hashlib.sha256()
 SECRET_CODE = b'\xed\x8a\x05A\xad\xfc\xd4\xd8%\x0f\xbf\xd5\xee\x07\xe5$\x03\xae\xf5\x05\xa0\x80\xb2\xf3)\x9cqP\xd0\x90=['
